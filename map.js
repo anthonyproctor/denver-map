40bb7b9b3183a6911897744ff68586d35a23c1a6
Anthony J. Proctor <anthonyproctor@gmail.com>
2017-02-28 14:18:51 -0700

    Feat: Added search.
    This feature has added a search box and functionality for the user to search the map with autocomplete.

--- map.js
+++ map.js
@@ -6,11 +6,15 @@ var markers = [];
 // This global polygon variable is to ensure only ONE polygon is rendered.
 var polygon = null;
 
+// Create placemarkers array to use in multiple functions to have control
+// over the number of places that show.
+var placeMarkers = [];
+
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 39.7392044, lng: -104.9902439},
-    zoom: 13
+    zoom: 12
   });
 
   // This autocomplete is for use in the search within time entry box.
@@ -21,6 +25,11 @@ function initMap() {
       document.getElementById('zoom-to-area-text'));
   //Bias the boundaries within the map for the zoom to area text.
   zoomAutocomplete.bindTo('bounds', map);
+  // Create a searchbox in order to execute a places search
+  var searchBox = new google.maps.places.SearchBox(
+      document.getElementById('places-search'));
+  // Bias the searchbox to within the bounds of the map.
+  searchBox.setBounds(map.getBounds());
 
   //These are the location listings that will be shown to the user.
   //Normally these are in a database.
@@ -66,7 +75,9 @@ function initMap() {
     });
   }
   document.getElementById('show-listings').addEventListener('click', showListings);
-  document.getElementById('hide-listings').addEventListener('click', hideListings);
+  document.getElementById('hide-listings').addEventListener('click', function() {
+    hideMarkers(markers);
+  });
   document.getElementById('toggle-drawing').addEventListener('click', function() {
     toggleDrawing(drawingManager);
   });
@@ -76,6 +87,16 @@ function initMap() {
   document.getElementById('search-within-time').addEventListener('click', function() {
     searchWithinTime();
   });
+
+  // Listen for the event fired when the user selects a prediction from the
+  // picklist and retrieve more details for that place.
+  searchBox.addListener('places_changed', function() {
+    searchBoxPlaces(this);
+  });
+  // Listen for the event fired when the user selects a prediction and clicks
+  // "go" more details for that place.
+  document.getElementById('go-places').addEventListener('click', textSearchPlaces);
+
   // Add an event listener so that the polygon is captured,  call the
   // searchWithinPolygon function. This will show the markers in the polygon,
   // and hide any outside of it.
@@ -155,7 +176,7 @@ function showListings() {
   map.fitBounds(bounds);
 }
 // This function will loop through the listings and hide them all.
-function hideListings() {
+function hideMarkers(markers) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
@@ -330,4 +351,65 @@ function displayDirections(origin) {
       window.alert('Directions request failed due to ' + status);
     }
   });
+}
+
+// This function fires when the user selects a searchbox picklist item.
+// It will do a nearby search using the selected query string or place.
+function searchBoxPlaces(searchBox) {
+ hideMarkers(placeMarkers);
+ var places = searchBox.getPlaces();
+ // For each place, get the icon, name and location.
+ createMarkersForPlaces(places);
+ if (places.length == 0) {
+   window.alert('We did not find any places matching that search!');
+ }
+}
+// This function firest when the user select "go" on the places search.
+// It will do a nearby search using the entered query string or place.
+function textSearchPlaces() {
+ var bounds = map.getBounds();
+ hideMarkers(placeMarkers);
+ var placesService = new google.maps.places.PlacesService(map);
+ placesService.textSearch({
+   query: document.getElementById('places-search').value,
+   bounds: bounds
+ }, function(results, status) {
+   if (status === google.maps.places.PlacesServiceStatus.OK) {
+     createMarkersForPlaces(results);
+   }
+ });
+}
+// This function creates markers for each place found in either places search.
+function createMarkersForPlaces(places) {
+ var bounds = new google.maps.LatLngBounds();
+ for (var i = 0; i < places.length; i++) {
+   var place = places[i];
+   var icon = {
+     url: place.icon,
+     size: new google.maps.Size(35, 35),
+     origin: new google.maps.Point(0, 0),
+     anchor: new google.maps.Point(15, 34),
+     scaledSize: new google.maps.Size(25, 25)
+   };
+   // Create a marker for each place.
+   var marker = new google.maps.Marker({
+     map: map,
+     icon: icon,
+     title: place.name,
+     position: place.geometry.location,
+     id: place.id
+   });
+   // If a marker is clicked, do a place details search on it in the next function.
+   marker.addListener('click', function() {
+   getPlacesDetails(this, place);
+   });
+   placeMarkers.push(marker);
+   if (place.geometry.viewport) {
+     // Only geocodes have viewport.
+     bounds.union(place.geometry.viewport);
+   } else {
+     bounds.extend(place.geometry.location);
+   }
+ }
+ map.fitBounds(bounds);
 }
